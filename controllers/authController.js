76af09b88d20843cb5a58c71fc0b8a196731be42
Aryaardhra <ardhra455@gmail.com>
2023-11-06 16:04:30 +0530

    client added

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,8 +43,6 @@ export const login = async (req, res, next) => {
        res.cookie("access_token", token, {
         httpOnly: true,
        }).status(200).json({...otherDetails});
-       
-
     } catch (error){
         next(error);
     }
